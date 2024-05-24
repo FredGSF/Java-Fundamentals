825f6667fc76d729276a930e45f2a2417e11b765
Frederico Fernandes <frederico.garcia.2000@gmail.com>
2024-05-24 15:32:54 +0100

    Update Days-Calculator2.0.java

--- Days-Calculator2.0.java
+++ Days-Calculator2.0.java
@@ -7,7 +7,7 @@ public class DaysCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
 
-        // Prompt the user to enter the first date 
+        // Prompt the user to enter the first date  
         System.out.print("Enter the first date (yyyy-MM-dd): ");  
         String dateString1 = scanner.nextLine();
         LocalDate date1 = LocalDate.parse(dateString1, DateTimeFormatter.ISO_LOCAL_DATE); 
