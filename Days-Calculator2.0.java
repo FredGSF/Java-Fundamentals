2c3fe6918a7740235cc57e326e70edc4c53f5a72
Frederico Fernandes <frederico.garcia.2000@gmail.com>
2024-05-24 15:32:33 +0100

    Update Days-Calculator2.0.java

--- Days-Calculator2.0.java
+++ Days-Calculator2.0.java
@@ -15,7 +15,7 @@ public class DaysCalculator {
         // Prompt the user to enter the second date
         System.out.print("Enter the second date (yyyy-MM-dd): ");
         String dateString2 = scanner.nextLine();
-        LocalDate date2 = LocalDate.parse(dateString2, DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate date2 = LocalDate.parse(dateString2, DateTimeFormatter.ISO_LOCAL_DATE); 
 
         // Calculate the difference between the two dates
         long daysBetween = ChronoUnit.DAYS.between(date1, date2);
